18ee76496a65b7858d7a41730ec7e18c6f690c7a
Davud44 <davud.seyfullayev44@gmail.com>
2021-03-02 16:51:43 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'WebRTC lets learn together'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _localRenderer = new RTCVideoRenderer();
+  MediaStream _localStream;
+  @override
+  dispose() {
+    _localStream.dispose();
+    _localRenderer.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    initRenderers();
+    _getUserMedia();
+    super.initState();
+  }
+
+  initRenderers() async {
+    await _localRenderer.initialize();
+  }
+
+  _getUserMedia() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': false,
+      'video': {
+        'mandatory': {
+          'minWidth':
+          '1280', // Provide your own width, height and frame rate here
+          'minHeight': '720',
+          'minFrameRate': '30',
+        },
+        'facingMode': 'user',
+        'optional': [],
+      },
+    };
+
+    _localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+
+    _localRenderer.srcObject = _localStream;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // body: Container(
+      //   child: new Stack(
+      //     children: <Widget>[
+      //       new Positioned(
+      //         top: 0.0,
+      //         right: 0.0,
+      //         left: 0.0,
+      //         bottom: 0.0,
+      //         child: new Container(child: new RTCVideoView(_localRenderer)),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return Center(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: RTCVideoView(_localRenderer, mirror: true),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
